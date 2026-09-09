5c57c39ab8a1644c3745f3b2690ed0126a00a0cc
Prince <princeoboesam@gmail.com>
2026-09-09 22:24:56 +0000

    Use only one embedding model

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,28 +1,18 @@
-# from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import OpenAIEmbeddings
-from langchain_ollama import OllamaEmbeddings
 from .config import OPENROUTER_API_KEY
 
 
-def get_embedding_model(model_name: str ='nomic'):
-    if model_name == 'nomic':
-        embedding_model = OllamaEmbeddings(
-            model='nomic-embed-text-v2-moe'
-        )
+def get_embedding_model(model_name: str ="openai/text-embedding-3-small"):
 
-    elif model_name == 'openai':
-        embedding_model = OpenAIEmbeddings(
-            model="openai/text-embedding-3-small",
-            api_key=OPENROUTER_API_KEY,
-            base_url="https://openrouter.ai/api/v1",
-        )
-
-    else:
-        raise ValueError(f"Unknown model: {model_name}")
+    embedding_model = OpenAIEmbeddings(
+        model=model_name,
+        api_key=OPENROUTER_API_KEY,
+        base_url="https://openrouter.ai/api/v1",
+    )
 
     print(model_name, 'was used for embeddings')
 
     return [embedding_model, model_name]
 
 
-embeddings = get_embedding_model(model_name='openai')
+embeddings = get_embedding_model()
